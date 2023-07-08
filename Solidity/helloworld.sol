4ad0c0d641675097efa566a9fdae57990e6fcac5
Esmeralda Godinez <esmegod19@gmail.com>
2023-07-07 20:23:19 -0600

    hello world modified

--- Solidity/helloworld.sol
+++ Solidity/helloworld.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-contract HelloWorld {
-    function sayHelloWorld() public pure returns (string memory) {
-        return "Hello World";
-    }
+pragma solidity ^0.8.17;
+
+contract Helloworld {
+    string public greet = "Hello World!";g
 }
